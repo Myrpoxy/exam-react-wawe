fb4f99c77fb5501e119efa571afef67c08afbc1b
Myrpoxy <113546999+Myrpoxy@users.noreply.github.com>
2025-03-17 18:32:08 +0300

    add photos in section

--- src/components/Galery/Galery.jsx
+++ src/components/Galery/Galery.jsx
@@ -1,7 +1,17 @@
 import './Galery.css';
+import React, { useState } from 'react';
 
 function Galery() {
     // const photoIDs =[ {id:1}, {id:2}, {id:3}, {id:4}, {id:5}, {id:6}, ];
+    const [selectedCategory, setSelectedCategory] = useState('icon');
+    const handleCategoryClick = (category) => { setSelectedCategory(category); };
+
+    // const photos = {
+    //     all: Array.from({ length: 6 }, (_, index) => `/img/galery-icon${index + 1}.jpg`),
+    //     tourists: Array.from({ length: 6 }, (_, index) => `/img/galery-tourists${index + 1}.jpg`),
+    //     nature: Array.from({ length: 6 }, (_, index) => `/img/galery-nature${index + 1}.jpg`),
+    //     profi: Array.from({ length: 6 }, (_, index) => `/img/galery-profi${index + 1}.jpg`),
+    // };
 
     return(
         <>
@@ -13,17 +23,21 @@ function Galery() {
 
                     <nav className="galery__menu">
                         <ul className="galery__list">
-                            <li className="galery__item"><a href="#" class="galery__link">ВСЕ</a></li>
-                            <li className="galery__item"><a href="#" class="galery__link">ТУРИСТЫ</a></li>
-                            <li className="galery__item"><a href="#" class="galery__link">ПРИРОДА</a></li>
-                            <li className="galery__item"><a href="#" class="galery__link">ПРОФИ</a></li>
+                            {/* <li className="galery__item" onClick={() => handleCategoryClick('all')}><a href="#" class="galery__link">ВСЕ</a></li>
+                            <li className="galery__item" onClick={() => handleCategoryClick('tourists')}><a href="#" class="galery__link">ТУРИСТЫ</a></li>
+                            <li className="galery__item" onClick={() => handleCategoryClick('nature')}><a href="#" class="galery__link">ПРИРОДА</a></li>
+                            <li className="galery__item" onClick={() => handleCategoryClick('profi')}><a href="#" class="galery__link">ПРОФИ</a></li> */}
+                            <li className="galery__item" onClick={() => handleCategoryClick('icon')}><p>ВСЕ</p></li>
+                            <li className="galery__item" onClick={() => handleCategoryClick('tourists')}><p>ТУРИСТЫ</p></li>
+                            <li className="galery__item" onClick={() => handleCategoryClick('nature')}><p>ПРИРОДА</p></li>
+                            <li className="galery__item" onClick={() => handleCategoryClick('profi')}><p>ПРОФИ</p></li>
                         </ul>
                     </nav>
 
                     <div className="galery__grid">
                         { 
                             Array.from({length: 6}, (_, index) => <div key={index} className={`galery__g-item item${index + 1}`}>
-                                <img src={`/img/galery-icon${index + 1}.jpg`} alt=""/>
+                                <img src={`/img/galery-${selectedCategory}${index + 1}.jpg`} alt=""/>
                             </div> ) 
                         }
                         {/* <div className="galery__g-item item1">
